34e277ef71deae2edde98c345bc6d7b62a39a787
legend048 <pklegend048@gmail.com>
2024-02-25 20:46:54 +0530

    media folder loction fixed

--- treasure_hunt/settings.py
+++ treasure_hunt/settings.py
@@ -118,14 +118,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'treasure_hunt' / 'static'
 ]
 
-Media_URL = 'media/'
-# Media_ROOT = BASE_DIR / 'treasure_hunt' /'treasure_hunt' / 'media'
-Media_ROOT = os.path.join(BASE_DIR, 'treasure_hunt', 'treasure_hunt', 'media')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'treasure_hunt'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
